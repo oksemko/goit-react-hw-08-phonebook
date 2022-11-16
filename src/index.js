814274e3036925ab79e98c7aaa64528561e49d99
Oksana Semko <osemko@gmail.com>
2022-11-17 00:43:03 +0200

    Added PersistGate & HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { HashRouter } from 'react-router-dom';
 
 import { App } from 'components/App/App';
 import { store } from './components/redux/store';
@@ -9,8 +11,12 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
