f21e5e56dd9ea8017ac41d6ee1e238df9ca3ef10
Oksana Semko <osemko@gmail.com>
2022-11-18 01:59:48 +0200

    Added contacts-operations

---
+++ src/components/redux/contacts/contacts-operations.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import {
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+} from './contacts-actions';
+
+const addContact =
+  ({ name, number }) =>
+  dispatch => {
+    const contact = {
+      name,
+      number,
+    };
+
+    dispatch(addContactRequest());
+
+    axios
+      .post('/contacts', contact)
+      .then(({ data }) => dispatch(addContactSuccess(data)))
+      .catch(error => dispatch(addContactError(error.message)));
+  };
+
+const fetchContacts = () => async dispatch => {
+  dispatch(fetchContactsRequest());
+
+  try {
+    const { data } = await axios.get('/contacts');
+
+    dispatch(fetchContactsSuccess(data));
+  } catch (error) {
+    dispatch(fetchContactsError(error.message));
+  }
+};
+
+const deleteContact = contactId => dispatch => {
+  dispatch(deleteContactRequest());
+
+  axios
+    .delete('/contacts/${contactId}')
+  .then(() =>)
+};
