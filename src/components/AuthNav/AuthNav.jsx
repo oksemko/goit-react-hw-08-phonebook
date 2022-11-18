b570bbbbef7806ba18ec6aadb71eee7549770ecf
Oksana Semko <osemko@gmail.com>
2022-11-18 04:08:53 +0200

    Added AuthNav

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import style from './AuthNav.styled';
+
+
+export default function AuthNav() {
+  return (
+    <div>
+      <NavLink
+        to="/register"
+        className={({ isActive }) =>
+          isActive ? `${style.activeLink}` : `${style.link}`
+        }
+      >
+        Registration
+      </NavLink>
+      <NavLink
+        to="login"
+        className={({ isActive }) =>
+          isActive ? `${style.activeLink}` : `${style.link}`
+        }
+      >
+        Login
+      </NavLink>
+    </div>
+  );
+}
