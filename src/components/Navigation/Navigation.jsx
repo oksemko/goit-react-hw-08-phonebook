595dccb746de5b8750b2a794f283e850e329d4a0
Oksana Semko <osemko@gmail.com>
2022-11-19 06:02:15 +0200

    Added Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { authSelectors } from "redux/auth/auth-selectors";
+
+import style from './Navigation.module.css';
+
+
+export const Navigation = () => {
+  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? `${style.activeLink}` : `${style.link}`
+        }
+      >
+        Main
+      </NavLink>
+
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          className={({ isActive }) =>
+            isActive ? `${style.activeLink}` : `${style.link}`
+          }
+        >
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+}
