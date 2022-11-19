f9192ae0265f40408af107713da9a13a23af3e4e
Oksana Semko <osemko@gmail.com>
2022-11-19 05:28:30 +0200

    Processed

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,39 +1,29 @@
-import {useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { ContactsItem } from '../ContactsItem/ContactsItem';
-import { Loader } from 'components/Loader/Loader';
-import { ColorRing } from 'react-loader-spinner';
+import { selectFilteredContacts } from 'redux/contacts/contacts-selectors';
+import { contactsOperations } from 'redux/contacts/contacts-operations';
 
 import { List } from './ContactsList.styled';
 
 
-export const ContactsList = ({ contacts: { data: contacts, isFetching, isError } }) => {
-  const filterValue = useSelector(state => state.filter);
+export const ContactsList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(contactsOperations.fetchContacts()),
+    [dispatch]);
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
-    <>
-      {isFetching && (
-        <>
-          <ColorRing
-          height={200}
-          width={200}
-          ariaLabel="blocks-loading"
+    <List>
+      {filteredContacts.map(({ id, name, number }) => {
+        return (
+          <ContactsItem
+            contact={{ id, name, number }}
+            key={id}
+            onDelete={id => dispatch(contactsOperations.deleteContact(id))}
           />
-          <Loader />
-        </>
-      )}
-      {!isFetching && !isError && contacts && (
-        <List>
-          {contacts
-            .filter(({ name }) =>
-              name.toLowerCase().includes(filterValue.toLowerCase()),
-            )
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .map(({ id, name, number }) => {
-              return <ContactsItem contact={{ id, name, number }} key={id} />;
-            })}
-        </List>
-      )}
-      {isError && <h1>Data are not found</h1>}
-    </>
+        );
+      })}
+    </List>
   );
-};
+}
