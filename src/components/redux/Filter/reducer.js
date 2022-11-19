e90e2c71558871f310c5a90d486b7a99efe2307c
Oksana Semko <osemko@gmail.com>
2022-11-19 05:43:32 +0200

    Relocated

--- src/components/redux/Filter/reducer.js
+++
@@ -1,6 +0,0 @@
-import { createReducer } from '@reduxjs/toolkit';
-import { changeFilter } from './actions';
-
-export const filter = createReducer('', {
-  [changeFilter]: (state, { payload }) => payload,
-});
--- src/components/redux/auth/auth-selectors.js
+++
@@ -1,8 +0,0 @@
-const selectIsLoggedIn = state => state.auth.isLoggedIn;
-
-const selectUserName = state => state.auth.user.name;
-
-export const authSelectors = {
-  selectIsLoggedIn,
-  selectUserName,
-};
