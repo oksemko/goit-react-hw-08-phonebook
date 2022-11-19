51e5751960fbde2fbbdadf34c2f40319ca803fa3
Oksana Semko <osemko@gmail.com>
2022-11-19 05:41:56 +0200

    Processed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { HashRouter } from 'react-router-dom';
 
-import { App } from 'components/App/App';
+import { App } from 'App/App';
 import { store } from './components/redux/store';
 
 import './index.css';
