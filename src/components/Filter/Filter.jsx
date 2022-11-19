ace53dbdb15388f0a9df2aeaa0e3e70ebf43678f
Oksana Semko <osemko@gmail.com>
2022-11-19 05:29:37 +0200

    Processed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeFilter } from 'components/redux/Filter/actions';
+import { changeFilter } from 'redux/contacts/contacts-actions';
 
 import { Container, Label, Input } from './Filter.styled';
 
 
 export const Filter = ({ title }) => {
-  const value = useSelector(state => state.filter);
+  const value = useSelector(state => state.contacts.filter);
 
   const dispatch = useDispatch();
 
@@ -15,7 +15,8 @@ export const Filter = ({ title }) => {
     <Container>
     <Label>
       <span>{title}</span>
-      <Input type="text"
+        <Input
+        type="text"
         name="filter"
         value={value}
         onChange={event => dispatch(changeFilter(event.currentTarget.value))}
