a80396976a836a2f6785d3161c39ce2422508b4e
Oksana Semko <osemko@gmail.com>
2022-11-20 15:38:56 +0200

    Processed

---
+++ src/views/HomeView.js
@@ -0,0 +1,19 @@
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    frontWeight: 500,
+    frontSize: 48,
+    textAlign: 'center',
+  },
+};
+
+const HomeView = () => (
+  <div style={styles.container}>
+    <h1 style={styles.title}>Phonebook</h1>
+  </div>
+);
---
+++ src/views/RegisterView.js
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from '../redux/auth';
+
+const styles = {
+  form: {
+    width: 320,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+};
+
+export default function RegisterView() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h1>Registration Page</h1>
+
+      <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+        <label style={styles.label}>
+          Name
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+
+        <label style={styles.label}>
+          Email
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label style={styles.label}>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
