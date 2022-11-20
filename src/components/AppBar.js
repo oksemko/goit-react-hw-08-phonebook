51391f11044762b92e8621880be033f049201e09
Oksana Semko <osemko@gmail.com>
2022-11-20 15:24:54 +0200

    Processed

---
+++ src/components/AppBar.js
@@ -0,0 +1,25 @@
+import { useSelector } from 'react-redux';
+import { Navigation } from './Navigation/Navigation.jsx';
+import { UserMenu } from './UserMenu';
+import { AuthNav } from './AuthNav/AuthNav.jsx';
+import { authSelectors } from 'redux/auth/auth-selectors.js';
+
+const styles = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottom: '1px solid #2A363B',
+    marginBottom: '15px',
+  },
+};
+
+export default function AppBar() {
+  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+  return (
+    <header style={styles.header}>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </header>
+  );
+}
