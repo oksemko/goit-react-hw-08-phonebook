a3c9827cba20070da22f6b08d7ce34fe5bcd88e6
Oksana Semko <osemko@gmail.com>
2022-12-26 02:49:41 +0200

    Added ExitIcon

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,19 +2,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { authSelectors, authOperations } from 'redux/auth';
 import defaultAvatar from './avatar-default.png';
 
-import { Container, Image, Span, Button } from './UserMenu.styled';
+import { Container, Image, Span, Button, ExitIcon } from './UserMenu.styled';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
-  const name = useSelector(authSelectors.selectUserName);
+  const name = useSelector(authSelectors.selectUsername);
   const avatar = defaultAvatar;
 
   return (
     <Container>
       <Image img src={avatar} alt=""></Image>
-      <Span>Welcome, {name}</Span>
+      <Span>Hello, {name}!</Span>
       <Button type="button" onClick={() => dispatch(authOperations.logOut())}>
-        Sign out
+        {/* Sign out */}
+        <ExitIcon />
       </Button>
     </Container>
   );
--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { ImExit } from 'react-icons/im';
+
 
 export const Container = styled.div`
 display: flex;
@@ -28,22 +30,47 @@ color: #2AA5A0;
 `
 
 export const Button = styled.button`
-display: block;
-border-radius: 10px;
-font-size: 14px;
-padding: 10px;
-margin: 0 auto;
-    width: 180px;
-    background-color: white;
-    cursor: pointer;
-    box-shadow: 0px 1px 2px 0px rgba(0, 65, 200, 0.5),
-            1px 2px 4px 0px rgba(0,65,200,0.5),
-            2px 4px 8px 0px rgba(0,65,200,0.5),
-            2px 4px 16px 0px rgba(0,65,200,0.5);
+ position: relative;
+  top: 0;
+  width: 60px;
+  height: 60px;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 2em;
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  border-right: 1px solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(4px);
+  z-index: 1;
+  box-shadow: 0 5px 45px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  transition: top 0.5s, z-index 0s, transform 0.5s;
+  transition-delay: 0.5s, 0.5s, 0s;
+  &:hover {
+    transition-delay: 0s, 0.5s, 0.5s;
+    top: -10px;
+    transform: translateY(10px);
+    z-index: 11;
+  }
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50px;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.5);
+    transform: skewX(45deg) translateX(150px);
+    transition: 0.5s;
+  }
+  &:hover::before {
+    transform: skewX(45deg) translateX(-150px);
+  }
+`;
 
-  {&}:hover{
-  background-color: #102770;
-  color: #c4c3ca;
-  box-shadow: 0 8px 24px 0 rgba();
-}
-`
+export const ExitIcon = styled(ImExit)``;
