86a74ded3f2fe537794dbed2ebec01d9345f3f73
Oksana Semko <osemko@gmail.com>
2022-12-26 02:59:47 +0200

    Added changes

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Navigation } from '../Navigation/Navigation.jsx';
 import UserMenu from '../UserMenu/UserMenu.jsx';
-import AuthNav from '../AuthNav/AuthNav.jsx';
+import { AuthNav } from '../AuthNav/AuthNav.jsx';
 import { authSelectors } from 'redux/auth';
 
 import { Header } from './AppBar.styled.jsx';
