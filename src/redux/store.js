bd8a8a0f2dea88c13972fb42ed16bf1e2b1ee008
Oksana Semko <osemko@gmail.com>
2022-12-26 03:13:14 +0200

    Added changes after review

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,45 +15,27 @@ import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reducer';
 import authSlice from './auth/auth-slice';
 
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+// Persisting token field from auth slice to localstorage
 const authPersistConfig = {
   key: 'auth',
   storage,
-  whilelist: ['token'],
+  whitelist: ['token'],
 };
 
-// const middleWare = [
-//   ...getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
-
-// const store = configureStore({
-//   reducer: {
-//     auth: persistReducer(authPersistConfig, authSlice.reducer),
-//     contacts: contactsReducer,
-//   },
-//   middleWare,
-//   devTools: process.env.NODE_ENV === 'development',
-// });
-
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authSlice.reducer),
     contacts: contactsReducer,
   },
-  // middleware,
-  // devTools: process.env.NODE_ENV === 'development',
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  middleware,
 });
 
-const persistor = persistStore(store);
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { store, persistor };
+export const persistor = persistStore(store);
