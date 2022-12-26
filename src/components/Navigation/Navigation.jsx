74a32cfc5728be6cfec63b178695b78aefad86a2
Oksana Semko <osemko@gmail.com>
2022-12-26 02:57:20 +0200

    Added HomeIcon & ContactsIcon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,35 +1,61 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import { authSelectors } from "redux/auth";
-
-import style from './Navigation.module.css';
-
+import {
+  NavigationList,
+  NavigationItem,
+  NavigationLink,
+  HomeIcon,
+  ContactsIcon,
+} from './Navigation.styled';
 
 export const Navigation = () => {
-  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
-
   return (
-    <nav>
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? `${style.activeLink}` : `${style.link}`
-        }
-      >
-        Main
-      </NavLink>
-
-      {isLoggedIn && (
-        <NavLink
-          to="/contacts"
-          className={({ isActive }) =>
-            isActive ? `${style.activeLink}` : `${style.link}`
-          }
-        >
-          Contacts
-        </NavLink>
-      )}
-    </nav>
+    <NavigationList>
+      <NavigationItem>
+        <NavigationLink to="/">
+          <HomeIcon />
+        </NavigationLink>
+      </NavigationItem>
+      <NavigationItem>
+        <NavigationLink to="/contacts">
+          <ContactsIcon />
+        </NavigationLink>
+      </NavigationItem>
+    </NavigationList>
   );
-}
+};
+
+
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { NavLink } from "react-router-dom";
+// import { authSelectors } from "redux/auth";
+
+// import style from './Navigation.module.css';
+
+
+// export const Navigation = () => {
+//   const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+
+//   return (
+//     <nav>
+//       <NavLink
+//         to="/"
+//         className={({ isActive }) =>
+//           isActive ? `${style.activeLink}` : `${style.link}`
+//         }
+//       >
+//         Main
+//       </NavLink>
+
+//       {isLoggedIn && (
+//         <NavLink
+//           to="/contacts"
+//           className={({ isActive }) =>
+//             isActive ? `${style.activeLink}` : `${style.link}`
+//           }
+//         >
+//           Contacts
+//         </NavLink>
+//       )}
+//     </nav>
+//   );
+// }
